c17676c5581d24c8edd8f0bebec40fcb286a4651
Cam Pegg <cam@campegg.com>
2024-01-28 14:18:58 -0500

    Refactor Feedbin feed fetching

--- utilities/external/feedbin.py
+++ utilities/external/feedbin.py
@@ -1,47 +1,59 @@
 #!/usr/bin/env python3
-
-import json
 import httpx
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import logging
 
 
-load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 
-data_dir = str(Path(__file__).parents[2]) + "/data/blogroll/"
+data_dir = Path(__file__).resolve().parents[2] / "data" / "blogroll"
+env_path = Path(__file__).resolve().parents[2] / ".env"
+load_dotenv(env_path)
+
 
 feedbin_user = os.getenv("FEEDBIN_USER")
 feedbin_pass = os.getenv("FEEDBIN_PASS")
 feedbin_api = "https://api.feedbin.com/v2"
-feedbin_subs = "subscriptions.json"
-feedbin_tags = "taggings.json"
+feedbin_endpoints = {"subs": "subscriptions.json", "tags": "taggings.json"}
 
 
-def get_data(type):
-    if type == "subs":
-        data = f"{feedbin_api}/{feedbin_subs}"
-        file = f"{data_dir}/{feedbin_subs}"
-    elif type == "tags":
-        data = f"{feedbin_api}/{feedbin_tags}"
-        file = f"{data_dir}/{feedbin_tags}"
-
+def fetch_json(endpoint):
     try:
-        response = httpx.get(data, auth=(feedbin_user, feedbin_pass))
-        payload = response.text
-    except Exception as e:
-        return f"Feedbin {type} update failed because {e} :("
+        response = httpx.get(endpoint, auth=(feedbin_user, feedbin_pass))
+        response.raise_for_status()
+        return response.text
+    except httpx.RequestError as e:
+        logging.error(f"Feedbin: request error {e}")
+    except httpx.HTTPStatusError as e:
+        logging.error(f"Feedbin: HTTP error {e}")
+    return None
 
-    try:
-        output_file = Path(file)
-        with output_file.open("w+") as target:
-            output_file.write_text(payload)
-        return f"Feedbin {type} update complete :)"
-    except Exception as e:
-        return f"Feedbin {type} update failed because {e} :("
+
+def write_json(file_path, data):
+    if data:
+        try:
+            with file_path.open("w") as target:
+                target.write(data)
+            return True
+        except IOError as e:
+            logging.error(f"Feedbin: file write error {e}")
+    return False
+
+
+def get_feeds(data_type):
+    endpoint = f"{feedbin_api}/{feedbin_endpoints[data_type]}"
+    file_path = data_dir / feedbin_endpoints[data_type]
+
+    data = fetch_json(endpoint)
+    if write_json(file_path, data):
+        return f"Feedbin {data_type} update complete :)"
+    else:
+        return f"Feedbin {data_type} update failed :("
 
 
 if __name__ == "__main__":
-    print(get_data("subs"))
-    print(get_data("tags"))
+    for data_type in feedbin_endpoints:
+        print(get_feeds(data_type))

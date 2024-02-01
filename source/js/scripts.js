683656b60b0bbdf0127d8fbd0d62bf12f066bba3
Cam Pegg <cam@campegg.com>
2024-01-31 21:28:13 -0500

    Simplify javascript, remove time-based theme switcher

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,103 +2,62 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    // color scheme switcher
-    const isDaytime = () => { // maybe update this to more accurately detect sunrise/sunset at some point?
-        const hour = new Date().getHours();
-        return hour >= 7 && hour < 19;
-    };
+    // color theme switcher
     const themeToggle = document.getElementById("theme-toggle");
     const themeIcon = document.getElementById("theme-icon");
+    let currentTheme = localStorage.getItem("theme") || "auto";
 
-    // check if 'theme' exists in localstorage, if not set it to default 'auto'
-    let currentTheme = localStorage.getItem("theme");
-    if (!currentTheme) {
-        currentTheme = "auto";
-        localStorage.setItem("theme", currentTheme);
-    }
+    localStorage.setItem("theme", currentTheme);
 
-    // get svg icons
     const fetchSVG = async (theme) => {
         const response = await fetch(`/assets/img/${theme}.svg`);
-        const text = await response.text();
-        return text;
+        return response.text();
     };
 
     const applyTheme = async (theme) => {
-        let newTheme = theme;
-        let iconTheme = theme; // separate variable for the icon theme
-
-        if (theme === "auto") {
-            newTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-        } else if (theme === "time") {
-            newTheme = isDaytime() ? "light" : "dark";
-        }
-
+        let newTheme = theme === "auto" ? (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light") : theme;
         document.querySelector('link[id="theme"]').href = `/assets/css/${newTheme}.css`;
-        const svgText = await fetchSVG(iconTheme); // fetch the svg for the icontheme
-        themeIcon.innerHTML = svgText;
+        const iconTheme = theme; // 'auto', 'dark', or 'light'
+        themeIcon.innerHTML = await fetchSVG(iconTheme);
     };
 
     themeToggle.addEventListener("click", async () => {
-        if (currentTheme === "auto") {
-            currentTheme = "time";
-        } else if (currentTheme === "time") {
-            currentTheme = "dark";
-        } else if (currentTheme === "dark") {
-            currentTheme = "light";
-        } else {
-            currentTheme = "auto";
-        }
+        currentTheme = currentTheme === "auto" ? "dark" : currentTheme === "dark" ? "light" : "auto";
         localStorage.setItem("theme", currentTheme);
         await applyTheme(currentTheme);
     });
 
-    const systemThemeListener = window.matchMedia("(prefers-color-scheme: dark)");
-    systemThemeListener.addListener(() => {
+    window.matchMedia("(prefers-color-scheme: dark)").addListener(() => {
         if (currentTheme === "auto") {
             applyTheme("auto");
         }
     });
 
-    // apply the theme based on the current setting
     applyTheme(currentTheme);
 
-    // external links open in new tab
-    let links = document.querySelectorAll("a");
-    for (let i = 0; i < links.length; i++) {
-        if (links[i].hostname !== window.location.hostname) {
-            links[i].setAttribute("target", "_blank");
-            let currentRel = links[i].getAttribute("rel");
-            if (currentRel) {
-                if (!currentRel.includes("noopener")) {
-                    links[i].setAttribute("rel", currentRel + " noopener");
-                }
-            } else {
-                links[i].setAttribute("rel", "noopener");
-            }
-        }
-    }
 
-    // set table cell spacing and padding
+    // open links in new tabs
+    const links = document.querySelectorAll("a");
+    links.forEach(link => {
+        if (link.hostname !== window.location.hostname) {
+            link.target = "_blank";
+            link.rel = link.rel.includes("noopener") ? link.rel : `${link.rel} noopener`.trim();
+        }
+    });
+
+
+    // set table cellpadding and cellspacing to 0
     const tables = document.querySelectorAll("table");
-    if (tables.length > 0) {
-        tables.forEach((table) => {
-            table.setAttribute("cellspacing", "0");
-            table.setAttribute("cellpadding", "0");
-        });
-    }
+    tables.forEach(table => {
+        table.setAttribute("cellspacing", "0");
+        table.setAttribute("cellpadding", "0");
+    });
+
 
     // add shadow to header on scroll
     const headerElement = document.querySelector("header");
     const handleScroll = () => {
-        const scrollPosition = window.scrollY;
-        const threshold = headerElement.offsetHeight;
-
-        if (scrollPosition >= threshold) {
-            headerElement.classList.add("shadow");
-        } else {
-            headerElement.classList.remove("shadow");
-        }
+        headerElement.classList.toggle("shadow", window.scrollY >= headerElement.offsetHeight);
     };
     window.addEventListener("scroll", handleScroll);
     handleScroll();

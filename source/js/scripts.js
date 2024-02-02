88bd8486896fa343eefec62f08c814c256314742
Cam Pegg <cam@campegg.com>
2024-02-02 17:10:44 -0500

    Make theme switcher only available when JS is enabled

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -3,36 +3,43 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     // color theme switcher
-    const themeToggle = document.getElementById("theme-toggle");
-    const themeIcon = document.getElementById("theme-icon");
+    const navList = document.querySelector('nav ul');
+    
+    const themeToggle = document.createElement('li');
+    themeToggle.id = "theme-toggle";
+    
+    const themeIcon = document.createElement('span');
+    themeIcon.id = "theme-icon";
+    themeIcon.className = "icon-svg";
+    
+    themeToggle.appendChild(themeIcon);
+    navList.appendChild(themeToggle);
+    
     let currentTheme = localStorage.getItem("theme") || "auto";
-
-    localStorage.setItem("theme", currentTheme);
-
+    
     const fetchSVG = async (theme) => {
         const response = await fetch(`/assets/img/${theme}.svg`);
         return response.text();
     };
-
+    
     const applyTheme = async (theme) => {
         let newTheme = theme === "auto" ? (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light") : theme;
         document.querySelector('link[id="theme"]').href = `/assets/css/${newTheme}.css`;
-        const iconTheme = theme; // 'auto', 'dark', or 'light'
-        themeIcon.innerHTML = await fetchSVG(iconTheme);
+        themeIcon.innerHTML = await fetchSVG(theme);
     };
-
+    
     themeToggle.addEventListener("click", async () => {
         currentTheme = currentTheme === "auto" ? "dark" : currentTheme === "dark" ? "light" : "auto";
         localStorage.setItem("theme", currentTheme);
         await applyTheme(currentTheme);
     });
-
+    
     window.matchMedia("(prefers-color-scheme: dark)").addListener(() => {
         if (currentTheme === "auto") {
             applyTheme("auto");
         }
     });
-
+    
     applyTheme(currentTheme);
 
 
